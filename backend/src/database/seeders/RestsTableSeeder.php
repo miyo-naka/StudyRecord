60bca1615c585b324f4557d3e33405f89625ea1b
miyokonakada <fortissimo_pikkoro@yahoo.co.jp>
2025-05-09 12:32:57 +1000

    fix: refactored seederfiles

--- backend/src/database/seeders/RestsTableSeeder.php
+++ backend/src/database/seeders/RestsTableSeeder.php
@@ -13,25 +13,19 @@ class RestsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $param = [
+        $param = [[
             'study_session_id' => 1,
             'rest_start_time' => '2025-04-1 11:30:00',
             'rest_finish_time' => '2025-04-1 12:00:00',
-        ];
-        DB::table('rests')->insert($param);
-
-        $param = [
+        ], [
             'study_session_id' => 1,
             'rest_start_time' => '2025-04-1 13:00:00',
             'rest_finish_time' => '2025-04-1 14:00:00',
-        ];
-        DB::table('rests')->insert($param);
-
-        $param = [
+        ],[
             'study_session_id' => 2,
             'rest_start_time' => Carbon::now(),
             'rest_finish_time' => Carbon::now(),
-        ];
+        ]];
         DB::table('rests')->insert($param);
     }
 }
